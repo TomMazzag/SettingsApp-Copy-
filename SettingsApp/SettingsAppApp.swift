4a3167ae9c2a2892ef5a52438faff56d80905604
TomMazzag <itstomazz@gmail.com>
2023-11-09 14:12:24 +0000

    Initial Commit

---
+++ SettingsApp/SettingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsAppApp.swift
+//  SettingsApp
+//
+//  Created by Tom Mazzag on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SettingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
